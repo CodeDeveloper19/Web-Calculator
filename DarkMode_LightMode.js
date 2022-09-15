6a12a388460ed2834aa0b7c90d13f3313a4f61c3
unknown <okoli555aka@gmail.com>
2022-09-15 02:09:46 +0100

    compressed the image sizes

--- DarkMode_LightMode.js
+++ DarkMode_LightMode.js
@@ -6,12 +6,12 @@ function changetheme(){
     
     if (lala.classList[1] == "fa-sun") {
         lala.classList.replace("fa-sun", "fa-moon")
-        body.style.backgroundImage = "url(Argon.jpg)"
+        body.style.backgroundImage = "url(Argon.png)"
         calculator.style.backgroundColor = "blueviolet"
         document.getElementById("result").style.color = "black"
     } else {
         lala.classList.replace("fa-moon", "fa-sun")
-        body.style.backgroundImage = "url(Telegram.jpg)"
+        body.style.backgroundImage = "url(Telegram.png)"
         calculator.style.backgroundColor = "lightblue"
         document.getElementById("result").style.color = "white"
     }
